00df05a12c8607e2c82e56be13f3b478627c6a2a
aokomoriuta <internal@enmps.net>
2020-03-08 20:53:52 +0900

    Keys for http://eliotoolsja.php.xdomain.jp/index.php

--- includes/app_credentials.php
+++ includes/app_credentials.php
@@ -1,6 +1,6 @@
 <?php
 	// Set client key and secret
-	$client_key = "3EZPQCWMPTP0TLV4SJNPOLMWJB4UVCBGMADXWQCYFU3MPIQZ";
-	$client_secret = "J2310KS05Z50PU44DUC0T0HPEYM2CEQKBBPROAGXMBACZRZG";
+	$client_key = "NGDGTKF4IYPNFZADVS4K4GDO12X3GENFGD3KOL05CGX0CZRW";
+	$client_secret = "VZ50R551RPPVRTQXTAFQF5BWFZHZVP14RSU2W3SOIFK3NIXF";
 	$redirect_uri = "http://eliotoolsja.php.xdomain.jp/index.php";
 ?>
